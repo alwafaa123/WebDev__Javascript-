009e5d6caf5148394d5fd0657283a90e86b46daf
alwafaa123 <alwafaafattahul@gmail.com>
2026-04-24 14:03:09 +0700

    javascript

---
+++ tipe_data.js/script.js
@@ -0,0 +1,24 @@
+// tipe data "pada java script
+let data_string = "dimas"; // tipe data string
+console.log(typeof data_string);
+console.log(data_string);
+
+let dataNamber = 16.57; // tipe data number
+console.log(dataNamber);
+console.log(typeof dataNamber);
+
+let dataBoolean = false; // tipe data boolean
+console.log(dataBoolean);
+console.log(typeof dataBoolean);
+
+datastring = "2"; // tipe data string
+console.log(datastring);
+console.log(typeof datastring);
+
+let dataKosong; // tipe data undepened
+
+console.log(dataKosong);
+console.log(typeof dataKosong);
+dataKosong = "herman";
+console.log(dataKosong);
+console.log(typeof dataKosong);
