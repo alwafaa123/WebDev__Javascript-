9d5b4785adcdc69b4f7d105452281f3f53b48da2
alwafaa123 <alwafaafattahul@gmail.com>
2026-04-26 10:41:06 +0700

    boolean

---
+++ tipedata-boolean/script.js
@@ -0,0 +1,15 @@
+// tipe data boolean
+let dat_bool1 = false;
+console.log(dat_bool1);
+
+let data_bool2 = 3 < 5; // ini yang dimaksud dengan boolean
+console.log(data_bool2);
+
+let data_bool3 = "asa" == "otong"; // ini yang dimaksud dengan boolean
+console.log(data_bool3);
+
+let data_bool4 = false == 0;
+console.log(data_bool4);
+
+let data_bool5 = false == 0n;
+console.log(data_bool5);
