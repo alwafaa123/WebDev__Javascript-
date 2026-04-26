583bd6617c6d2618eb4207ccfc7281cae03b1916
alwafaa123 <alwafaafattahul@gmail.com>
2026-04-26 11:14:15 +0700

    NaN-andefined

---
+++ NAN $ andefined/script.js
@@ -0,0 +1,17 @@
+// NAN dan undefined
+
+// NaN = nOT a  Number
+console.log(`akar dari -1: ${Math.sqrt(-1)}`);
+console.log(`"ucup" / "otong": ${"ucup" / "otong"}`);
+console.log(`"ucup" * "otong": ${"ucup" * "otong"}`);
+console.log(`"ucup" + "otong": ${"ucup" + "otong"}`);
+
+let data = -parseInt("belajar abc");
+console.log(data + 5); // nan ini tidak bisa din operasiakan letterSpacing:
+
+// undefined ini belum mengetahui nilainya
+
+let a;
+console.log("a = ${a}");
+console.log(Math.sqrt(4));
+console.log(console.log("TESTING CODE"));
