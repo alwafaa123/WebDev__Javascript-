69a20613fc2799b1bc2188b79774caccc305cf93
alwafaa123 <alwafaafattahul@gmail.com>
2026-04-27 16:02:09 +0700

    aritmatika

---
+++ New folder/script.js
@@ -0,0 +1,38 @@
+// operator aritmatika jika ke duanya adalah angka
+
+//  expresi dan statement +-/*
+// % <-- modulo
+// ** <-- pangkat atau power
+// ++ <-- incremet
+// -- <--decrement
+
+let x = 10;
+console.log(x + 1.5);
+console.log(x / 1);
+console.log(x * 1);
+console.log(x - 1);
+
+// aritmatika khusus di javscript
+console.log(x % 4); // sisa pembagian
+console.log(x ** 2); // dipangkatkan
+
+// pemograman biasa
+x = x + 1;
+console.log(x);
+x = x + 1;
+console.log(x);
+x++;
+x++;
+console.log(x);
+
+x = x - 1;
+console.log(x);
+
+x--;
+console.log(x);
+
+// study kasus
+let y = 1 + "1";
+console.log(y);
+y = 1 + parseInt("1");
+console.log(y);
