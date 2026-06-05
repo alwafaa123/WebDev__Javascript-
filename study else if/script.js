108cdd5fe321c85c7fbf76fb2041498ddbf4eb44
alwafaa123 <alwafaafattahul@gmail.com>
2026-06-05 21:19:05 +0700

    study swich case

---
+++ study else if/script.js
@@ -0,0 +1,22 @@
+console.log("sabrina");
+let nama = "paijo";
+
+if (nama == "otong") {
+  console.log("nama saya otong sangar");
+} else if (nama == "dede") {
+  console.log("nama saya dede ganteng");
+} else {
+  console.log("saya adalah bukan siapa - siapa saja");
+}
+
+switch (nama) {
+  case "otong":
+    console.log("nama saya otong sangar");
+    break;
+  case "dede":
+    console.log("nama saya dede ganteng");
+    break;
+  default:
+    console.log("switch case no bady");
+    break;
+}
