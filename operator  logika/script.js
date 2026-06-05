88abb291fbc692af7b0bfbd1a49245cfd27a3456
alwafaa123 <alwafaafattahul@gmail.com>
2026-06-05 20:51:05 +0700

    add initial HTML and JavaScript files for if statement demonstration

---
+++ operator  logika/script.js
@@ -0,0 +1,9 @@
+let nama = "Dimas";
+console.log("nama" + "=" + nama);
+
+if (nama === "Dimas") {
+  console.log("nama benar");
+} else {
+  console.log("nama salah akhiri program code akau sebel sama kamu");
+}
+console.log("salah nama bang");
